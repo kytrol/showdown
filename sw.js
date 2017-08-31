7a57e61aef25180abe6f909a062aaf1ea046fc9a
KyTrol <kt.trout@gmail.com>
2017-08-31 09:43:45 -0400

    Basic service worker

---
+++ sw.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const CACHE_NAME = 'showdown-v0';
+const urlsToCache = ['/', '/index.html', '/build/bundle.js'];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .catch(err => console.error(err))
+  );
+});
+
+const BASE_URL = 'http://api.tvmaze.com/search/shows?q=';
+self.addEventListener('fetch', evt => {
+  if (evt.request.url.indexOf(BASE_URL) > -1) {
+    evt.respondWith(
+      caches.open(CACHE_NAME)
+        .then(cache => (
+          fetch(evt.request)
+            .then(res => {
+              cache.put(evt.request.url, res.clone());
+              return res;
+            })
+        ))
+        .catch(err => console.error(err))
+    );
+  } else {
+    evt.respondWith(
+      caches.match(evt.request)
+      .then(res => {
+        if (res) {
+          return res;
+        }
+
+        return fetch(evt.request);
+      })
+      .catch(err => console.error(err))
+    );
+  }
+});
