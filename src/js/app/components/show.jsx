5925598596cd5e6d2c1eb5d059a2a2efa7f3898e
KyTrol <kt.trout@gmail.com>
2017-09-08 10:56:42 -0400

    Show list styles

--- src/js/app/components/show.jsx
+++ src/js/app/components/show.jsx
@@ -4,9 +4,19 @@ const Show = ({ showInfo }) => {
   const { show } = showInfo;
   return (
     <li class='show'>
+      <div class='network'>
+        {show.network && <h5>{show.network.country.code}</h5>}
+        {show.network && <h5>{show.network.name}</h5>}
+      </div>
       <h3>{show.name}</h3>
-      {show.image && <img src={show.image.medium} />}
-      <p dangerouslySetInnerHTML={{ __html: show.summary }} />
+      <div class='show-img'>
+        {show.image && <img src={show.image.medium} alt={show.image.name}/>}
+      </div>
+      {show.genres && (
+        <ul class='genres'>
+          {show.genres.filter((g, i) => i < 2).map(g => <li>{g}</li>)}
+        </ul>
+      )}
     </li>
   );
 };
