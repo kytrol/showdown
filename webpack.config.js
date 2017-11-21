dafd29ed264663cc6f4b3d2114ab340bdf807fce
KyTrol <kt.trout@gmail.com>
2017-11-21 17:10:21 -0500

    Accept addons property in webpack config to extend env configurations

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,15 @@
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack/webpack.common');
 
+const buildAddons = addonsArg => {
+  const addons = [].concat(...[addonsArg]).filter(Boolean);
+
+  return addons.map(addon => require(`./webpack/addons/webpack.${addon}`));
+};
+
 module.exports = envOpts => {
-  const { env } = envOpts;
+  const { env, addons } = envOpts;
   const envConfig = require(`./webpack/webpack.${envOpts.env}`);
 
-  return webpackMerge(commonConfig(env), envConfig);
+  return webpackMerge(commonConfig(env), envConfig, ...buildAddons(addons));
 };
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -69,6 +69,7 @@ module.exports = env => ({
       'process.env': {
         NODE_ENV: JSON.stringify(env)
       }
-    })
+    }),
+    new webpack.ProgressPlugin()
   ]
 });
